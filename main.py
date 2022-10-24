9e18cff4e2135bff8c3e8c309c7333fd2e23e925
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-24 15:25:07 +0000

    Repl broke lol

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hi")
+"""import discord
+import os # default module
+from keep_alive import keep_alive
+
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+@bot.slash_command(name = "hello", description = "Say hello to the bot")
+async def hello(ctx):
+    await ctx.respond("Hey!")
+
+keep_alive()
+bot.run(os.environ["token"]) # run the bot with the token"""
+print("yo")
