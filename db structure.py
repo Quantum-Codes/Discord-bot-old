9b55742ec7c9c90b0e95cdc45e8b0007cc56bcdd
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2022-10-24 12:59:20 +0000

    Start unknown bot v2

--- db structure.py
+++
@@ -1,8 +0,0 @@
-"""this is a place where i note down the structure of replit database manually.
-this file isn't used anywhere. it has .py for automatically adding indents"""
-
-
-db = {
-  "reminded": "<bool>" #for bumping
-  "lastbump": "<int>" #for bumping
-}
